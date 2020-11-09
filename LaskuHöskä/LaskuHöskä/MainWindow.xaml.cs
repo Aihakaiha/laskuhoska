d4e1ea083d76be72c3d2f90e5af6e5693cbff332
Chic <57492147+Aihakaiha@users.noreply.github.com>
2020-11-09 11:59:00 +0200

    Add files via upload

---
+++ LaskuHöskä/LaskuHöskä/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LaskuHöskä
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+        }
+
+        private bool check(int x, int y)
+        {
+            if (x == y) return true;
+            return false;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int l1 = 0, l2 = 0;
+
+            try
+            {
+                l1 = int.Parse(luku1.Text);
+                l2 = int.Parse(luku2.Text);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            tulos.Content = check(l1, l2).ToString();
+        }
+    }
+}
